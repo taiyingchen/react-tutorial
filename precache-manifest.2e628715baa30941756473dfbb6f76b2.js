fe7d3ead024f181836f60d3feb36f48d18d772d6
Taiying Chen <taiying.tychen@gmail.com>
2019-10-23 23:56:54 -0500

    Updates

---
+++ precache-manifest.2e628715baa30941756473dfbb6f76b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "109dc9241d0192092c320e794a69c7c9",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "baa80dcef5dc4edd0d18",
+    "url": "/react-tutorial/static/css/main.ad8343fb.chunk.css"
+  },
+  {
+    "revision": "f44ec3f62f7c05e9ffd9",
+    "url": "/react-tutorial/static/js/2.8ca14d58.chunk.js"
+  },
+  {
+    "revision": "baa80dcef5dc4edd0d18",
+    "url": "/react-tutorial/static/js/main.3e0aa2b5.chunk.js"
+  },
+  {
+    "revision": "acd1605a6af8f99b39b9",
+    "url": "/react-tutorial/static/js/runtime-main.b18dd318.js"
+  }
+]);
